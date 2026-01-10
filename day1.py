365eb40f5308372f1142af90faf283d84ee961cd
shubham phuke <sphukepatil@gmail.com>
2026-01-10 02:47:51 +0000

    day1 to day5 problems

--- day1.py
+++ day1.py
@@ -129,3 +129,9 @@ if __name__=="__main__":
     k=3
     print(minimum_inrease(lis9,k))            
 '''TENTH PROBLEM : Given an array of n integers. We need to reduce size of array to one. We are allowed to select a pair of integers and remove the larger one of these two. This decreases the array size by 1. Cost of this operation is equal to value of smallest one. Find out minimum sum of costs of operations needed to convert the array into a single element.'''
+def min_cost(lis10):
+    n=len(lis10)
+    return ((n-1)*min(lis10))
+if __name__=="__main__":
+    lis10=[4,3,2,3]
+    print(min_cost(lis10))    
